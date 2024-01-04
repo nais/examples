0f450bd0c6bef2f4cebab33b68d7866965322580
Geir André Lund <geir.andre.lund@nav.no>
2024-01-04 08:20:28 +0100

    Export data via gRPC using OTLP format

--- shop-frontend/instrumentation.node.ts
+++ shop-frontend/instrumentation.node.ts
@@ -1,5 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 import { OTLPExporterNodeConfigBase } from '@opentelemetry/otlp-exporter-base';
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
