c150d6044e48fb0c8f70d2f4ab24ed914795f8d8
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-07-05 13:58:33 +0200

    Do not flake out when .env file does not exist

--- flaky-service/main.go
+++ flaky-service/main.go
@@ -19,7 +19,7 @@ func init() {
 
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file", err)
+		slog.Info("Error loading .env file", "error", err)
 	}
 
 	if err := envconfig.Process(ctx, &c); err != nil {
