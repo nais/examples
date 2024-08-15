b136dd8b6e1f0bad5b0ca31ecc5e26bc2b35f2e9
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-08-15 09:09:55 +0200

    Add trace_id to flaky-frontend logs

--- flaky-frontend/src/app/api/flaky/route.ts
+++ flaky-frontend/src/app/api/flaky/route.ts
@@ -1,14 +1,33 @@
 import getConfig from 'next/config';
 import type { NextRequest } from 'next/server';
+import pino from 'pino';
+import api from '@opentelemetry/api'
 
 const { serverRuntimeConfig: c } = getConfig();
 
 export async function GET(request: NextRequest) {
+  let current_span = api.trace.getSpan(api.context.active());
+  let trace_id = current_span?.spanContext().traceId;
+
+  const logger = pino({
+    base: undefined,
+    formatters: {
+      level: (label) => {
+        return {
+          level: label
+        }
+      }
+    }
+  }).child({ trace_id });
+  logger.info('GET /api/flaky');
+
+  logger.info('Calling flaky service');
   return await fetch(c.flakyServiceUrl, { cache: 'no-store' })
     .then((response) => {
+      logger.info('Flaky service response', response);
       return response;
     }).catch((error) => {
-      console.error(error?.cause?.message);
+      logger.error('Flaky service error', error);
       return new Response(
         JSON.stringify({
           error: error?.cause?.message || "Internal Server Error",
