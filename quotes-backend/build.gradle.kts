d74808cc653f14cea5215938fa8b2953167f9d06
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-05-20 11:50:31 +0200

    Bump quotes-backend dependencies

--- quotes-backend/build.gradle.kts
+++ quotes-backend/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.4"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
+    id("io.ktor.plugin") version "2.3.12" // Reverted Ktor plugin version
     application
 }
 
@@ -18,27 +18,29 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty:2.3.4") // Netty engine for Ktor
-    implementation("io.ktor:ktor-server-core:2.3.4")
-    implementation("io.ktor:ktor-server-openapi:2.3.4") // OpenAPI support
-    implementation("io.ktor:ktor-server-swagger:2.3.4") // Swagger UI support
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.4")
-    implementation("io.ktor:ktor-server-status-pages:2.3.4") // Added StatusPages dependency
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("io.ktor:ktor-server-netty:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-server-core:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-server-openapi:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-server-swagger:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-server-status-pages:2.3.12") // Reverted Ktor version
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12") // Reverted Ktor version
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     // Explicitly define Logback version to ensure consistency
-    implementation("ch.qos.logback:logback-classic:1.3.14")
-    implementation("ch.qos.logback:logback-core:1.3.14")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("ch.qos.logback:logback-core:1.5.6")
 
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4") {
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1") {
         exclude(group = "ch.qos.logback") // Exclude all transitive Logback dependencies
     }
+    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.16.0-alpha") // OTel Logback MDC appender
 
-    testImplementation("io.ktor:ktor-server-tests:2.3.4")
-    testImplementation("io.ktor:ktor-client-content-negotiation:2.3.4") // Added for content negotiation in tests
-    testImplementation("io.ktor:ktor-client-mock:2.3.4") // Added for mocking client requests
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.10")
+    testImplementation("io.ktor:ktor-server-tests:2.3.12") // Reverted Ktor version
+    testImplementation("io.ktor:ktor-client-content-negotiation:2.3.12") // Reverted Ktor version
+    testImplementation("io.ktor:ktor-client-mock:2.3.12") // Reverted Ktor version
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.20")
 }
 
 tasks.withType<Test> {
